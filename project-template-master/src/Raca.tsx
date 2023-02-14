d9256e5112b02dc605b750d8b9f2c77570e2f106
z20ist17 <z20ist17@z00p3-06.SPSKNM>
2023-02-14 09:27:04 +0100

    CSS-ko - podstránka Raca.tsx

--- project-template-master/src/Raca.tsx
+++ project-template-master/src/Raca.tsx
@@ -4,16 +4,20 @@ import "./App.css";
 export default function Raca(){
     return(
 
-    
-        <div className="pozadieRaca" > 
-          <div className="RacaSTRLyziar"> <img src="src\images\RacaSTRLyziar.png"></img></div>
-          <div className="RacaSTRLogo"> <img src="src\images\RacaSTRStranka.png"></img></div>
+    <body>
+    <div id="pageFill" > 
+        <div className="RacaSTRLyziar"> <img src="src\images\RacaSTRLyziar.png"></img></div>
+        <div className="RacaSTRLogo"> <img src="src\images\RacaSTRStranka.png"></img></div>
+        <div className="RacaUzitocnyNadpis"><h1>Užitočné informácie o rezorte</h1> </div>
+        <div className="RacaInfoDiv"><p className="ppp">Textdsadsaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssssss ssssssssssssssssssssssssssssaaaaaaaaaaaaa ssssssssssasssssssssssssssssssssssss</p>
+            </div>
+        <div className="RecenzieDiv"></div>
+        <div className="GMaps"><button><img src="src\images\GMapsLogo.png"></img></button></div>
 
 
 
 
-
-          </div>
-
+    </div>
+    </body>
     );
 }
